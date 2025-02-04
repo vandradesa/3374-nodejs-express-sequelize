2837a3d0954b6883f590eec41c7fce1aa8e28ba7
Vanessa Andrade <v.andradd@gmail.com>
2025-02-04 14:11:30 -0300

    Organizando e criando Services...

---
+++ src/controllers/PessoaController.js
@@ -0,0 +1,15 @@
+const Controller = require('./Controller.js');
+const PessoaServices = require('../services/PessoaServices.js');
+
+const pessoaServices = new PessoaServices();
+
+
+class PessoaController extends Controller{
+
+  constructor() {
+    super(pessoaServices);       
+  }
+
+}
+
+module.exports = PessoaController;
